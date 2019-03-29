caab03a3935a207989540b32929f2757636d013a
Joel S <joel29dec@gmail.com>
2019-03-29 21:36:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,21 @@ def shift_with_args(arr, num)
 end
 
 def using_concat(arr1, arr2)
-  arr1
+  arr1.concat(arr2)
 end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten 
+end
+
+def using_delete(arr, int)
+  arr.delete(int)
+end
+
+
+  
+  
